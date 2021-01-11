64189a70e3ccea3194fb8de4ef926b5eb7b1f006
Matt Carr <mhcarr17@gmail.com>
2021-01-11 16:31:31 -0600

    changed data source

--- flask_app/app.py
+++ flask_app/app.py
@@ -18,8 +18,7 @@ class Energy(db.Model):
     __tablename__ = 'energy'
     id = db.Column(db.Integer, primary_key=True)
     time = db.Column(db.DateTime, nullable=False)
-    meter = db.Column(db.Float, nullable=False)
-    inverter = db.Column(db.Float, nullable=False)
+    energy = db.Column(db.Float, nullable=False)
 
 @app.route('/')
 def index():
